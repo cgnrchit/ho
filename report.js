4d863c891c0f9c3fa9d1f3d1fb5712223e17883c
cgnrchit <chengannurchit@gmail.com>
2024-11-29 16:10:36 +0530

    Update report.js

--- report.js
+++ report.js
@@ -26,43 +26,50 @@ function formatDate(dateString) {
 
 
 //report table------------------------------------------------------------------
-onValue(collectionListDB, function(snapshot){
-    if(snapshot.exists()){
-        let userArray = Object.entries(snapshot.val());
-        tblBodyEl.innerHTML ="";
-        for (let i = 0; i <userArray.length; i++){
-            let currentUser = userArray[i];
-            let currentUserid = currentUser[0];
-            let currentUserValue = currentUser[1];
+onValue(collectionListDB, function(snapshot) {
+  if (snapshot.exists()) {
+      let userArray = Object.entries(snapshot.val());
 
-            // Format dates---------------------------------------------------------------
-            const formattedDepositDate = formatDate(currentUserValue.depositDate);
-            const formattedReceiptDate = formatDate(currentUserValue.receiptDate);
-            // table data-----------------------------------------------------------------
-            tblBodyEl .innerHTML +=
-                        ` <tr>
-                          <td style="width: 9%; text-align: left;">${currentUserValue.hiddenBranchName}</td> 
-                          <td style="width: 8%; text-align: center;">${formattedDepositDate}</td>
-                          <td style="width: 179px; text-align: left;">${currentUserValue.staffName}</td>
-                          <td style="width: 8%; text-align: center;">${formattedReceiptDate}</td>
-                          <td style="width: 19%; text-align: left;">${currentUserValue.customerName}</td> 
-                          <td style="width: 7%; text-align: center;">${currentUserValue.chitNumber}</td> 
-                          <td style="width: 7%; text-align: center;">${currentUserValue.instalment}</td> 
-                          <td style="width: 7%; text-align: center;">${currentUserValue.receiptNumber}</td> 
-                          <td style="width: 7%; text-align: right;">${currentUserValue.amount}</td>
-                          <td style="width: 7%; text-align: right;">${currentUserValue.commission}</td>
-                          <td style="width: 7%; text-align: right;">${currentUserValue.bankAmount}</td>                    
-                          <td style="width: 4%; text-align: center;"><input class="checkbox" type="checkbox" name="select"></td>
-                          <td style="width: 4%; text-align: center;">
-                              <button class="btn-delete" data-id="${currentUserid}">
-                                  <img src="Image/delete.svg" class="del-icon">
-                              </button>
-                          </td>
-                      </tr>`
-        }
-    }else{
-        tblBodyEl .innerHTML = "<tr><td> No Record</tb></tr>";
-    }
+      // Sort the array by depositDate in ascending order
+      userArray.sort(([, userValueA], [, userValueB]) => {
+          const dateA = new Date(userValueA.depositDate);
+          const dateB = new Date(userValueB.depositDate);
+          return dateA - dateB; // Ascending order
+      });
+
+      let htmlContent = ""; // Accumulate HTML here
+
+      userArray.forEach(([userId, userValue]) => {
+          const formattedDepositDate = formatDate(userValue.depositDate);
+          const formattedReceiptDate = formatDate(userValue.receiptDate);
+
+          htmlContent += `
+              <tr>
+                  <td style="width: 9%; text-align: left;">${userValue.hiddenBranchName}</td> 
+                  <td style="width: 8%; text-align: center;">${formattedDepositDate}</td>
+                  <td style="width: 179px; text-align: left;">${userValue.staffName}</td>
+                  <td style="width: 8%; text-align: center;">${formattedReceiptDate}</td>
+                  <td style="width: 19%; text-align: left;">${userValue.customerName}</td> 
+                  <td style="width: 7%; text-align: center;">${userValue.chitNumber}</td> 
+                  <td style="width: 7%; text-align: center;">${userValue.instalment}</td> 
+                  <td style="width: 7%; text-align: center;">${userValue.receiptNumber}</td> 
+                  <td style="width: 7%; text-align: right;">${userValue.amount}</td>
+                  <td style="width: 7%; text-align: right;">${userValue.commission}</td>
+                  <td style="width: 7%; text-align: right;">${userValue.bankAmount}</td>                    
+                  <td style="width: 4%; text-align: center;"><input class="checkbox" type="checkbox" name="select"></td>
+                  <td style="width: 4%; text-align: center;">
+                      <button class="btn-delete" data-id="${userId}">
+                          <img src="delete.svg" class="del-icon">
+                      </button>
+                  </td>
+              </tr>`;
+      });
+
+      // Update DOM only once
+      tblBodyEl.innerHTML = htmlContent;
+  } else {
+      tblBodyEl.innerHTML = "<tr><td>No Record</td></tr>";
+  }
 });
 
 
